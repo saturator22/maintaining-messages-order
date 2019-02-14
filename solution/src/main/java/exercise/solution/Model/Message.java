13679b4e6d237d7810a5e01ae89e59cd026ab9f9
saturator22 <f.szczeski@gmail.com>
2019-02-14 10:54:00 +0100

    Implement Message model

---
+++ solution/src/main/java/exercise/solution/Model/Message.java
@@ -0,0 +1,23 @@
+package exercise.solution.Model;
+
+public class Message {
+
+    private long id;
+    private String message;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
