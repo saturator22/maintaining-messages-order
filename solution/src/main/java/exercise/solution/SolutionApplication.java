501618ead5b0105d61d4c63ce459dd3e0b39afb4
saturator22 <f.szczeski@gmail.com>
2019-02-14 10:49:24 +0100

    Append project structure

---
+++ solution/src/main/java/exercise/solution/SolutionApplication.java
@@ -0,0 +1,14 @@
+package exercise.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionApplication.class, args);
+	}
+
+}
+
