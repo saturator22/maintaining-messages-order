0c6b1e94a55d5f97b7ff90af5436bcacdedf8eb4
saturator22 <f.szczeski@gmail.com>
2019-02-14 14:52:21 +0100

    Add getMessages & clearResource methods and changing test resource to TreeSet

--- solution/src/main/java/exercise/solution/Repository/ResourceRepository.java
+++ solution/src/main/java/exercise/solution/Repository/ResourceRepository.java
@@ -3,15 +3,29 @@ package exercise.solution.Repository;
 import exercise.solution.Model.Message;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class ResourceRepository {
 
-    List<Message> messageList = new ArrayList<>();
+    Set<Message> messageSet = new TreeSet<>();
 
     public void appendMessage(Message message) {
-        this.messageList.add(message);
+        this.messageSet.add(message);
+    }
+
+    public List<Message> getMessages() {
+        List<Message> messageList = new ArrayList<>();
+        Iterator<Message> iterator = this.messageSet.iterator();
+
+        while(iterator.hasNext()) {
+            messageList.add(iterator.next());
+        }
+
+        return messageList;
+    }
+
+    public void empty() {
+        this.messageSet.clear();
     }
 }
