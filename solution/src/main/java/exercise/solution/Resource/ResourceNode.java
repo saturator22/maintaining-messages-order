02b594e670486ce2e0a61b3080373340b5ecedf8
saturator22 <f.szczeski@gmail.com>
2019-02-14 15:47:20 +0100

    Implement generic ResourceNode

---
+++ solution/src/main/java/exercise/solution/Resource/ResourceNode.java
@@ -0,0 +1,39 @@
+package exercise.solution.Resource;
+
+class ResourceNode<T extends Comparable<T>> {
+
+    public static final ResourceNodeColorEnum RED = ResourceNodeColorEnum.RED;
+    public static final ResourceNodeColorEnum BLACK = ResourceNodeColorEnum.BLACK;
+
+    //E.g. Message object
+    public T item;
+
+    //Counters of nodes under the given node leftLeaf/rightLeaf
+    int leftLeafsCounter;
+    int rightLeafsCounter;
+
+    //Node color
+    boolean nodeColor;
+
+    //References to neighbour nodes
+    ResourceNode<T> leftLeaf;
+    ResourceNode<T> rightLeaf;
+    ResourceNode<T> parentNode;
+
+    //Initial constructor
+    ResourceNode() {
+        leftLeaf = null;
+        rightLeaf = null;
+        parentNode = null;
+        nodeColor = RED.colorValue();
+        leftLeafsCounter = 0;
+        rightLeafsCounter = 0;
+    }
+
+    //Constructor to assign item
+    ResourceNode(T item) {
+        this();
+        this.item = item;
+    }
+
+}
