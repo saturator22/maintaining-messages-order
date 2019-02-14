a1decaf7387ec998664c58026c0bbe01a50d3aff
saturator22 <f.szczeski@gmail.com>
2019-02-14 10:50:24 +0100

    Add PostMapping for /messages endpoint

---
+++ solution/src/main/java/exercise/solution/Controller/MessageController.java
@@ -0,0 +1,20 @@
+package exercise.solution.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    @Autowired
+    MessageService messageService;
+
+    @PostMapping
+    public void storeMessage(@RequestBody Message message) {
+        messageService.storeMessage(message);
+    }
+}
