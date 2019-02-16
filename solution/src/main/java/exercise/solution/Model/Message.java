362e990c5d20e6702d1635a916e335ee054629bb
saturator22 <f.szczeski@gmail.com>
2019-02-16 15:56:44 +0100

    Add Overrided hash & equals methods

--- solution/src/main/java/exercise/solution/Model/Message.java
+++ solution/src/main/java/exercise/solution/Model/Message.java
@@ -1,5 +1,8 @@
 package exercise.solution.Model;
 
+
+import java.util.Objects;
+
 public class Message implements Messegeable, Comparable<Message>{
 
     private int id;
@@ -11,6 +14,29 @@ public class Message implements Messegeable, Comparable<Message>{
 
     public String getMessage() {
         return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return getId() == message1.getId() &&
+                Objects.equals(getMessage(), message1.getMessage());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getMessage());
     }
 
     @Override
