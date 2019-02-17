dc79caf49c8f4af812f40f438f7c315ebf39fc85
saturator22 <f.szczeski@gmail.com>
2019-02-17 22:03:22 +0100

    Set Messegeable as Abstract Class

--- solution/src/main/java/exercise/solution/Model/Message.java
+++ solution/src/main/java/exercise/solution/Model/Message.java
@@ -1,46 +1,6 @@
 package exercise.solution.Model;
 
 
-import java.util.Objects;
+public class Message extends Messageable{
 
-public class Message implements Messegeable, Comparable<Message>{
-
-    private int id;
-    private String message;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "id=" + id +
-                ", message='" + message + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Message message1 = (Message) o;
-        return getId() == message1.getId() &&
-                Objects.equals(getMessage(), message1.getMessage());
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(getId(), getMessage());
-    }
-
-    @Override
-    public int compareTo(Message otherMessage) {
-        return otherMessage == null ? 1 : this.id - otherMessage.getId();
-    }
 }
--- solution/src/main/java/exercise/solution/Model/Messageable.java
+++ solution/src/main/java/exercise/solution/Model/Messageable.java
@@ -1,5 +1,21 @@
 package exercise.solution.Model;
 
-public interface Messageable {
 
+public abstract class Messageable extends MessageableComparator{
+
+    private long timeStampMillis;
+    private String message;
+    private long offSet;
+
+    public long getTimeStampMillis() {
+        return timeStampMillis;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getOffSet() {
+        return offSet;
+    }
 }
