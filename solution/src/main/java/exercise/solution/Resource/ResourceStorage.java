688740163aa6e7f57f82dea2cf0744d609a41b21
saturator22 <f.szczeski@gmail.com>
2019-02-17 22:05:30 +0100

    Add insert method

---
+++ solution/src/main/java/exercise/solution/Resource/ResourceStorage.java
@@ -0,0 +1,69 @@
+package exercise.solution.Resource;
+
+import exercise.solution.Model.Messageable;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class ResourceStorage {
+
+    final static ResourceNode EMPTY = new ResourceNode(null, null);
+    AtomicMarkableReference<ResourceNode> head;
+
+    public ResourceStorage() {
+        this.head = new AtomicMarkableReference<>(EMPTY, false);
+    }
+
+//    private ResourceNode search(Messageable searchedKey, ResourceNode leftNode) {
+//        ResourceNode nextOfLeftNode;
+//        ResourceNode rightNode;
+//
+//        while(true) {
+//            ResourceNode current = head;
+//            ResourceNode currentNext = head.getNext();
+//
+//            AtomicReference<ResourceNode> cachedCurrent = new AtomicReference<>(current);
+//            AtomicReference<ResourceNode> cachedCurrentNext = new AtomicReference<>(currentNext);
+//
+//            while(current.getKey().compare())
+//
+//        }
+//    }
+
+    public boolean insert(Messageable message) {
+        //TODO REFACTOR OF MULTIPLIED PIECES OF CODE
+        boolean isInserted = false;
+        AtomicMarkableReference<ResourceNode> currentAtomicNode = head;
+
+        while(!isInserted) {
+            ResourceNode currentNode = currentAtomicNode.getReference();
+            if(head.getReference().key == null) {
+                ResourceNode nodeToInsert = new ResourceNode(message, EMPTY);
+                isInserted = head.compareAndSet(head.getReference(), nodeToInsert,false, false);
+            } else{
+                if(message.compare(message, currentNode.key) < 0 && !currentNode.next.isMarked()) {
+                    if(currentAtomicNode == head) {
+                        ResourceNode nextNode = currentAtomicNode.getReference();
+                        ResourceNode nodeToInsert = new ResourceNode(message, nextNode);
+                        isInserted = head.compareAndSet(head.getReference(), nodeToInsert, false, false);
+                    } else {
+                        ResourceNode nextNode = currentAtomicNode.getReference();
+                        ResourceNode nodeToInsert = new ResourceNode(message, nextNode);
+                        isInserted = currentAtomicNode.compareAndSet(currentNode, nodeToInsert,
+                                false, false);
+                    }
+                } else if(message.compare(message, currentNode.key) >= 0) {
+                    if(currentNode.key != null) {
+                        currentAtomicNode = currentNode.next;
+                    } else {
+                        ResourceNode nextNode = currentAtomicNode.getReference();
+                        ResourceNode newNode = new ResourceNode(message, nextNode);
+                        isInserted = currentAtomicNode.compareAndSet(nextNode, newNode, false, false);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    //TODO CAS()
+}
