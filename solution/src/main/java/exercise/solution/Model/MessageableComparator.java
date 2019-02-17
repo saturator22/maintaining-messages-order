90a79e5f008d15531c7bf094c6e4bf3ceb23aa1a
saturator22 <f.szczeski@gmail.com>
2019-02-17 22:03:45 +0100

    Implement comparator for Messegeable

---
+++ solution/src/main/java/exercise/solution/Model/MessageableComparator.java
@@ -0,0 +1,29 @@
+package exercise.solution.Model;
+
+import java.util.Comparator;
+
+public class MessageableComparator<T extends Messageable> implements Comparator<T> {
+
+    @Override
+    public int compare(T m1, T m2) {
+
+        if(m2 == null) {
+            return 1;
+        }
+
+        Long m1offSet = m1.getOffSet();
+        Long m2offSet = m2.getOffSet();
+
+        int offSetComp = m1offSet.compareTo(m2offSet);
+
+        if (offSetComp != 0) {
+            return offSetComp;
+        }
+
+        Long m1timeStamp = m1.getTimeStampMillis();
+        Long m2timeStamp = m2.getTimeStampMillis();
+
+        return m1timeStamp.compareTo(m2timeStamp);
+
+    }
+}
