ab990add59feae5edf931e50d942b446d825185d
saturator22 <f.szczeski@gmail.com>
2019-02-18 18:48:25 +0100

    Changing the way of sending response from /resource

--- solution/src/main/java/exercise/solution/Controller/MessageController.java
+++ solution/src/main/java/exercise/solution/Controller/MessageController.java
@@ -1,6 +1,7 @@
 package exercise.solution.Controller;
 
 import exercise.solution.Model.Message;
+import exercise.solution.Model.Messageable;
 import exercise.solution.Service.MessageService;
 import exercise.solution.Service.ResourceService;
 import org.slf4j.Logger;
@@ -26,23 +27,23 @@ public class MessageController {
     @Async("asyncWorker")
     @PostMapping("/message")
     public void storeMessage(@RequestBody Message message) {
-        log.info("STORE MESSAGE STARTED CONTROLLER");
+        log.info("STORE MESSAGE STARTED CONTROLLER WITH MESSAGE: " + message);
         resourceService.storeMessage(message);
     }
 
     @Async("asyncWorker")
     @GetMapping("/resource")
     public @ResponseBody
-    CompletableFuture<List<Message>> getMessages() {
-        log.info("GET MESSAGES CONTROLLER");
-        List<Message> messageList = resourceService.getMessages();
+    CompletableFuture<String> getMessages() {
+        List<Messageable> messageList = resourceService.getMessages();
+        log.info("GET MESSAGES CONTROLLER WITH STATE : " + messageList);
 
-        return CompletableFuture.completedFuture(messageList);
+        StringBuilder sb = new StringBuilder();
+        for(Messageable message1 : messageList) {
+            sb.append(message1.getMessage());
+        }
+
+        return CompletableFuture.completedFuture(sb.toString());
     }
 
-    @Async("asyncWorker")
-    @DeleteMapping("/empty")
-    public void clearMessages() {
-        resourceService.clearResource();
-    }
 }
