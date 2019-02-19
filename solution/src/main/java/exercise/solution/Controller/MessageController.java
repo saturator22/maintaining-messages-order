af06fd0c3ba494a6c3a5ca8b42ba82af40d0524d
saturator22 <f.szczeski@gmail.com>
2019-02-19 18:33:38 +0100

    Moving getMessages into storeMessage for test purposes

--- solution/src/main/java/exercise/solution/Controller/MessageController.java
+++ solution/src/main/java/exercise/solution/Controller/MessageController.java
@@ -1,8 +1,7 @@
 package exercise.solution.Controller;
 
-import exercise.solution.Model.Message;
-import exercise.solution.Model.Messageable;
-import exercise.solution.Service.MessageService;
+import exercise.solution.Model.Commit;
+import exercise.solution.Service.CommitService;
 import exercise.solution.Service.ResourceService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,14 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-public class MessageController {
+public class MessageController{
 
     @Autowired
-    MessageService messageService;
+    CommitService commitService;
 
     @Autowired
     ResourceService resourceService;
@@ -26,24 +24,19 @@ public class MessageController {
 
     @Async("asyncWorker")
     @PostMapping("/message")
-    public void storeMessage(@RequestBody Message message) {
-        log.info("STORE MESSAGE STARTED CONTROLLER WITH MESSAGE: " + message);
-        resourceService.storeMessage(message);
+    public @ResponseBody CompletableFuture<String> storeMessage(@RequestBody Commit commit) {
+        log.info("STORE MESSAGE STARTED CONTROLLER WITH MESSAGE: " + commit);
+        resourceService.storeMessage(commit);
+        return getMessages();
     }
 
     @Async("asyncWorker")
     @GetMapping("/resource")
     public @ResponseBody
     CompletableFuture<String> getMessages() {
-        List<Messageable> messageList = resourceService.getMessages();
-        log.info("GET MESSAGES CONTROLLER WITH STATE : " + messageList);
+        String messageList = resourceService.getMessages();
 
-        StringBuilder sb = new StringBuilder();
-        for(Messageable message1 : messageList) {
-            sb.append(message1.getMessage());
-        }
-
-        return CompletableFuture.completedFuture(sb.toString());
+        return CompletableFuture.completedFuture(messageList);
     }
 
 }
