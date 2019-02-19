0005433a5a17ae498bd1717e1b4b9605357bcd2d
saturator22 <f.szczeski@gmail.com>
2019-02-19 18:50:15 +0100

    Add OffSet as helper class for Commit

---
+++ solution/src/main/java/exercise/solution/Model/OffSet.java
@@ -0,0 +1,16 @@
+package exercise.solution.Model;
+
+//import exercise.solution.Comparator.OffSetComparator;
+
+public class OffSet {
+
+    private int offSet;
+
+    public int getOffSet() {
+        return offSet;
+    }
+
+    public void setOffSet(int offSet) {
+        this.offSet = offSet;
+    }
+}
