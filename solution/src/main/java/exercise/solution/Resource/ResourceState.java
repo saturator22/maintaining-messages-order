c73df29959de37feb87db440a6370e004b71d090
saturator22 <f.szczeski@gmail.com>
2019-02-20 14:10:52 +0100

    Add ResourceState as a main source of state

---
+++ solution/src/main/java/exercise/solution/Resource/ResourceState.java
@@ -0,0 +1,52 @@
+package exercise.solution.Resource;
+
+import exercise.solution.Model.Commit;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+@Component
+public class ResourceState {
+
+    StringBuffer state;
+
+    public ResourceState() {
+        this.state = new StringBuffer();
+    }
+
+    void updateState(AtomicMarkableReference<ResourceNode> currentNode) {
+
+        StringBuffer currentState = state;
+        currentState.setLength(0);
+
+        while(currentNode.getReference().key != null) {
+            Commit currentCommit = currentNode.getReference().key;
+
+            int offSet = currentCommit.getOffSet().getOffSet();
+            String message = currentCommit.getMessage().getMessage();
+
+            offSet = setOffSet(currentState, offSet);
+
+            currentState.insert(offSet, message);
+            currentNode = currentNode.getReference().next;
+        }
+        System.out.println(state);
+    }
+
+    private int setOffSet(StringBuffer currentState, int offSet) {
+
+        if(currentState.length() == 0) {
+            offSet = currentState.length();
+        } else if(currentState.length() < offSet) {
+            offSet = currentState.length();
+        } else if(offSet < 0) {
+            offSet = currentState.length() + offSet + 1;
+        }
+
+        return offSet;
+    }
+
+    public String getState() {
+        return state.toString();
+    }
+}
